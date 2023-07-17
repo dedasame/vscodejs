b3ff08ab7b48a7aaae923882c4738852f36779e8
dedasame <ecekarakoc36@gmail.com>
2023-07-17 15:35:24 +0300

    qwe

--- nesne.js
+++ nesne.js
@@ -1,4 +1,6 @@
 
+//object ->  key:value
+
 var user = {
     name: "Eren",
     age: 20,
@@ -12,6 +14,8 @@ var user = {
 
 }
 
+/*
+
 //properties erişim yolları
 console.log(user.name)
 console.log(user[`name`])
@@ -20,6 +24,18 @@ console.log(user.address.street)
 
 user.test()
 
-delete user.age // delete user["age"]   -> parantezli halinin avantajı? : 
+var input = "age"
+console.log(user[input]) // user.input -> undefined (input bulamıyor) !kapalı parantez kullandığı zaman age görüyor
+
+delete user.age // delete user["age"] 
 console.log(user)
+
+console.log(Object.keys(user)) //key döndürür
+console.log(Object.values(user)[2]) //value dizisinin indexiyle döndürür
+
+*/
+
+
+
+
 
