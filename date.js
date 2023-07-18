99a4a7c67b6ca34a5b7a84180afda9f81f19a1b5
dedasame <ecekarakoc36@gmail.com>
2023-07-18 14:56:12 +0300

    qwe

--- date.js
+++ date.js
@@ -1,8 +1,19 @@
 //DATE OBJECT
 
+var time = new Date() //UTC saatine göre veriyor (timestamp)
+var zaman = Date.now() // 1 Ocak 1970'den beri geçmiş ms 
+
+var result = time.getTime() //ms cinsinden 
+var ay = time.getMonth() //bulunduğun ayın indexi 0-11 
 
 
+var sonuc = time.toLocaleDateString("tr-TR",{day: "numeric",month: "long",year: "numeric",hour:"numeric",minute:"numeric"})
 
+console.log(time)
+console.log(zaman)
+console.log(ay) 
+
+console.log(sonuc)
 
 
 
